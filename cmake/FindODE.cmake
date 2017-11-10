85cd5465d2867ccd7bf5d6459d57f43668bf0b5e
rosebudflyaway <rosebudflyaway@gmail.com>
2017-11-09 17:08:40 -0800

    Fix indentation

--- cmake/FindODE.cmake
+++ cmake/FindODE.cmake
@@ -21,23 +21,23 @@
 # Find ODE Open Dynamics Engine
 
 find_path(ODE_INCLUDE_DIR ode/ode.h
-    /usr/include
-    /usr/local/include
+  /usr/include
+  /usr/local/include
 )
 
 set(ODE_NAMES ${ODE_NAMES} ode libode)
 find_library(ODE_LIBRARY NAMES ${ODE_NAMES} PATH)
 
 if(ODE_INCLUDE_DIR)
-    message(STATUS "Found ODE include dir: ${ODE_INCLUDE_DIR}")
+  message(STATUS "Found ODE include dir: ${ODE_INCLUDE_DIR}")
 else(ODE_INCLUDE_DIR)
-    message(STATUS "Couldn't find ODE include dir: ${ODE_INCLUDE_DIR}")
+  message(STATUS "Couldn't find ODE include dir: ${ODE_INCLUDE_DIR}")
 endif(ODE_INCLUDE_DIR)
 
 if(ODE_LIBRARY)
-    message(STATUS "Found ODE library: ${ODE_LIBRARY}")
+  message(STATUS "Found ODE library: ${ODE_LIBRARY}")
 else(ODE_LIBRARY)
-    message(STATUS "Couldn't find ODE library: ${ODE_LIBRARY}")
+  message(STATUS "Couldn't find ODE library: ${ODE_LIBRARY}")
 endif(ODE_LIBRARY)
 
 if(ODE_INCLUDE_DIR AND ODE_LIBRARY)
@@ -45,11 +45,11 @@ if(ODE_INCLUDE_DIR AND ODE_LIBRARY)
 endif(ODE_INCLUDE_DIR AND ODE_LIBRARY)
 
 if(ODE_FOUND)
-    message(STATUS "Looking for Open Dynamics Engine - found")
-    set(CMAKE_C_FLAGS "-DdSINGLE")
-    set(HAVE_ODE true)
-    include(IgnImportTarget)
-    ign_import_target(ODE)
+  message(STATUS "Looking for Open Dynamics Engine - found")
+  set(CMAKE_C_FLAGS "-DdSINGLE")
+  set(HAVE_ODE true)
+  include(IgnImportTarget)
+  ign_import_target(ODE)
   if(NOT ODE_FIND_QUIETLY)
     message(STATUS "Found ODE: ${ODE_LIBRARY}")
   endif(NOT ODE_FIND_QUIETLY)
