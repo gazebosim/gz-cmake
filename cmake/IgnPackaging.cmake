a447c0a9e0e9251b880d8d6af55e53a4b8d45b9b
Michael X. Grey <grey@osrfoundation.org>
2018-01-04 13:31:47 -0800

    Do not use the suffix for the basic version-checking file

--- cmake/IgnPackaging.cmake
+++ cmake/IgnPackaging.cmake
@@ -206,7 +206,7 @@ function(ign_create_cmake_package)
   # allow users of the library to specify the API or version to depend on
   write_basic_package_version_file(
     ${CMAKE_CURRENT_BINARY_DIR}/${ign_version_output}
-    VERSION "${PROJECT_VERSION_FULL}"
+    VERSION "${PROJECT_VERSION_FULL_NO_SUFFIX}"
     COMPATIBILITY SameMajorVersion)
 
   # Install the configuration files to the configuration installation directory
