e43a506caa14e91da2725c2e913914206e5a1606
Carlos Aguero <caguero@osrfoundation.org>
2018-01-10 23:15:40 +0100

    Add manual detection

--- cmake/FindYAML.cmake
+++ cmake/FindYAML.cmake
@@ -16,12 +16,42 @@
 ########################################
 # Find yaml.
 
-set(yaml_quiet_arg)
-if(YAML_FIND_QUIETLY)
-  set(yaml_quiet_arg QUIET)
-endif()
-
-find_package(yaml CONFIG ${yaml_quiet_arg})
-
 include(IgnPkgConfig)
-ign_pkg_config_entry(YAML "yaml >= ${YAML_FIND_VERSION}")
+ign_pkg_check_modules(YAML yaml-0.1)
+
+# If that failed, then fall back to manual detection.
+if(NOT YAML_FOUND)
+
+  if(NOT YAML_FIND_QUIETLY)
+    message(STATUS "Attempting manual search for yaml")
+  endif()
+
+  find_path(YAML_INCLUDE_DIRS yaml.h ${YAML_INCLUDE_DIRS} ENV CPATH)
+  find_library(YAML_LIBRARIES NAMES yaml)
+  set(YAML_FOUND true)
+
+  if(NOT YAML_INCLUDE_DIRS)
+    if(NOT YAML_FIND_QUIETLY)
+      message(STATUS "Looking for yaml headers - not found")
+    endif()
+    set(YAML_FOUND false)
+  endif()
+
+  if(NOT YAML_LIBRARIES)
+    if(NOT YAML_FIND_QUIETLY)
+      message (STATUS "Looking for yaml library - not found")
+    endif()
+    set(YAML_FOUND false)
+  endif()
+
+  if(YAML_FOUND)
+    include(IgnImportTarget)
+    ign_import_target(YAML)
+  endif()
+
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(
+    YAML
+    REQUIRED_VARS YAML_FOUND)
+
+endif()
