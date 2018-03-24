980763d88b75a0e378db60ff80b16fa17810097a
Jose Luis Rivero <jrivero@osrfoundation.org>
2018-03-24 20:15:38 +0100

    Support build_type in configure.bat. Default to Release

--- configure.bat
+++ configure.bat
@@ -4,12 +4,17 @@
 
 :: ign-cmake has no dependencies
 
+:: Set configuration variables
+@set build_type=Release
+@if not "%1"=="" set build_type=%1
+@echo Configuring for build type %build_type%
+
 :: Go to the directory that this file configure.bat file exists in
 cd /d %~dp0
 
 :: Create a build directory and configure
 md build
 cd build
-cmake .. -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX="%WORKSPACE_INSTALL_DIR%" -DBUILD_TESTING:BOOL=False
+cmake .. -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX="%WORKSPACE_INSTALL_DIR%" -DBUILD_TESTING:BOOL=False -DCMAKE_BUILD_TYPE="%build_type%"
 
 :: If the caller wants to build and/or install, they should do so after calling this script
