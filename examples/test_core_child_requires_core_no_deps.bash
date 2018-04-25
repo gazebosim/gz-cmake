3e488bdb6e1ab79953a1e7f9a29e773879dc4ff8
Steve Peters <scpeters@openrobotics.org>
2018-04-24 17:00:50 -0700

    add test to illustrate problem with pkg-config requires for ign-cmake1 package that depends on something that uses ign-cmake0

--- examples/test_core_child_requires_core_no_deps.bash
+++ examples/test_core_child_requires_core_no_deps.bash
@@ -1,12 +1,14 @@
 #!/bin/bash
-if pkg-config ignition-core_child --print-requires \
-  | grep ignition-core_no_deps
+no_deps=ignition-$1
+child=ignition-$2
+if pkg-config ${child} --print-requires \
+  | grep ${no_deps}
 then
-  echo Successfully detected ignition-core_no_deps requirement
+  echo Successfully detected ${no_deps} requirement
   cp core_child_requires_core_no_deps_pass.xml ../test_results/core_child_requires_core_no_deps.xml
   exit 0
 else
-  echo Could not detect ignition-core_no_deps requirement
+  echo Could not detect ${no_deps} requirement
   cp core_child_requires_core_no_deps_fail.xml ../test_results/core_child_requires_core_no_deps.xml
   exit 1
 fi
