50449b3b597438693d876b311c6acdff1f7eb75d
Jose Luis Rivero <jrivero@osrfoundation.org>
2018-06-04 01:00:45 +0200

    Support for WIN32

--- cmake/FindOGRE.cmake
+++ cmake/FindOGRE.cmake
@@ -44,8 +44,6 @@
 #                      VERSION 1.8.0
 #                      COMPONENTS RTShaderSystem Terrain Overlay)
 
-include(IgnPkgConfig)
-
 # Grab the version numbers requested by the call to find_package(~)
 set(major_version ${OGRE_FIND_VERSION_MAJOR})
 set(minor_version ${OGRE_FIND_VERSION_MINOR})
@@ -53,46 +51,51 @@ set(minor_version ${OGRE_FIND_VERSION_MINOR})
 # Set the full version number
 set(full_version ${major_version}.${minor_version})
 
-ign_pkg_check_modules_quiet(OGRE "OGRE >= ${full_version}")
+if (WIN32)
+	find_package(OGRE ${full_version} COMPONENTS ${OGRE_FIND_COMPONENTS})
+	message(FATAL_ERROR "OGRE_INCLUDE_DIRS ${OGRE_INCLUDE_DIRS}")
+else()
+	include(IgnPkgConfig)
+	ign_pkg_check_modules_quiet(OGRE "OGRE >= ${full_version}")
 
-if (OGRE_FOUND)
+	if (OGRE_FOUND)
 
-  # set OGRE major, minor, and patch version number
-  string (REGEX REPLACE "^([0-9]+).*" "\\1"
-    OGRE_VERSION_MAJOR "${OGRE_VERSION}")
-  string (REGEX REPLACE "^[0-9]+\\.([0-9]+).*" "\\1"
-    OGRE_VERSION_MINOR "${OGRE_VERSION}")
-  string (REGEX REPLACE "^[0-9]+\\.[0-9]+\\.([0-9]+).*" "\\1"
-    OGRE_VERSION_PATCH ${OGRE_VERSION})
+		# set OGRE major, minor, and patch version number
+		string (REGEX REPLACE "^([0-9]+).*" "\\1"
+			OGRE_VERSION_MAJOR "${OGRE_VERSION}")
+		string (REGEX REPLACE "^[0-9]+\\.([0-9]+).*" "\\1"
+			OGRE_VERSION_MINOR "${OGRE_VERSION}")
+		string (REGEX REPLACE "^[0-9]+\\.[0-9]+\\.([0-9]+).*" "\\1"
+			OGRE_VERSION_PATCH ${OGRE_VERSION})
 
-  # find ogre components
-  foreach(component ${OGRE_FIND_COMPONENTS})
-    ign_pkg_check_modules_quiet(OGRE-${component} "OGRE-${component} >= ${full_version}")
-    if(OGRE-${component}_FOUND)
-      list(APPEND OGRE_LIBRARIES OGRE-${component}::OGRE-${component})
-    elseif(OGRE_FIND_REQUIRED_${component})
-      set(OGRE_FOUND false)
-    endif()
-  endforeach()
+		# find ogre components
+		foreach(component ${OGRE_FIND_COMPONENTS})
+			ign_pkg_check_modules_quiet(OGRE-${component} "OGRE-${component} >= ${full_version}")
+			if(OGRE-${component}_FOUND)
+				list(APPEND OGRE_LIBRARIES OGRE-${component}::OGRE-${component})
+			elseif(OGRE_FIND_REQUIRED_${component})
+				set(OGRE_FOUND false)
+			endif()
+		endforeach()
 
-  # Also find OGRE's plugin directory, which is provided in its .pc file as the
-  # `plugindir` variable.  We have to call pkg-config manually to get it.
-  # On Windows, we assume that all the OGRE* defines are passed in manually
-  # to CMake.
-  if (NOT WIN32)
-    execute_process(COMMAND pkg-config --variable=plugindir OGRE
-                    OUTPUT_VARIABLE _pkgconfig_invoke_result
-                    RESULT_VARIABLE _pkgconfig_failed)
-    if(_pkgconfig_failed)
-      BUILD_WARNING ("Failed to find OGRE's plugin directory.  The build will succeed, but there will likely be run-time errors.")
-    else()
-      # This variable will be substituted into cmake/setup.sh.in
-      set (OGRE_PLUGINDIR ${_pkgconfig_invoke_result})
-    endif()
-  endif()
+		# Also find OGRE's plugin directory, which is provided in its .pc file as the
+		# `plugindir` variable.  We have to call pkg-config manually to get it.
+		# On Windows, we assume that all the OGRE* defines are passed in manually
+		# to CMake.
+		if (NOT WIN32)
+			execute_process(COMMAND pkg-config --variable=plugindir OGRE
+											OUTPUT_VARIABLE _pkgconfig_invoke_result
+											RESULT_VARIABLE _pkgconfig_failed)
+			if(_pkgconfig_failed)
+				BUILD_WARNING ("Failed to find OGRE's plugin directory.  The build will succeed, but there will likely be run-time errors.")
+			else()
+				# This variable will be substituted into cmake/setup.sh.in
+				set (OGRE_PLUGINDIR ${_pkgconfig_invoke_result})
+			endif()
+		endif()
 
-  set(OGRE_RESOURCE_PATH ${OGRE_PLUGINDIR})
-  # Seems that OGRE_PLUGINDIR can end in a newline, which will cause problems when
-  # we pass it to the compiler later.
-  string(REPLACE "\n" "" OGRE_RESOURCE_PATH ${OGRE_RESOURCE_PATH})
-endif ()
+		set(OGRE_RESOURCE_PATH ${OGRE_PLUGINDIR})
+		# Seems that OGRE_PLUGINDIR can end in a newline, which will cause problems when
+		# we pass it to the compiler later.
+		string(REPLACE "\n" "" OGRE_RESOURCE_PATH ${OGRE_RESOURCE_PATH})
+	endif ()
