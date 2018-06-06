7295d93d77f2da345ab8ecbfe63b8383e1738f76
Jose Luis Rivero <jrivero@osrfoundation.org>
2018-06-06 18:34:53 +0200

    Debug components

--- cmake/FindIgnOGRE.cmake
+++ cmake/FindIgnOGRE.cmake
@@ -54,6 +54,8 @@ set(full_version ${major_version}.${minor_version})
 if (WIN32)
   find_package(OGRE ${full_version}
                COMPONENTS ${IgnOGRE_FIND_COMPONENTS})
+  message(STATUS "COMPONENTS: ${IgnOGRE_FIND_COMPONENTS}")
+
   # The last subdirecty of OGRE_INCLUDE_DIRS from vcpkg FindOgre includes the
   # OGRE/ subdirectory while the code uses headers the form OGRE/header.h
   set(p_last_subdir)
