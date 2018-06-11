c39f9b624376aca61f6e4ec5b2ff18c16ad5752a
Jose Luis Rivero <jrivero@osrfoundation.org>
2018-06-11 18:31:27 +0200

    Support for Windows in FindGTS module

--- cmake/FindGTS.cmake
+++ cmake/FindGTS.cmake
@@ -15,5 +15,63 @@
 #
 ########################################
 # Find GNU Triangulation Surface Library
-include(IgnPkgConfig)
-ign_pkg_check_modules(GTS gts)
+
+if (NOT WIN32)
+  # Configuration using pkg-config modules
+  include(IgnPkgConfig)
+  ign_pkg_check_modules(GTS gts)
+else()
+  # true by default, change to false when a failure appears
+  set(GTS_FOUND true)
+
+  # 1. look for GTS headers
+  find_path(GTS_INCLUDE_DIRS gts.h
+    hints
+      ${CMAKE_FIND_ROOT_PATH}
+    paths
+      ${CMAKE_FIND_ROOT_PATH}
+    doc "GTS header include dir"
+    path_suffixes
+      include
+  )
+
+  if (GTS_INCLUDE_DIRS)
+    if(NOT GTS_FIND_QUIETLY)
+      message(STATUS "Looking for gts.h gtsconfig.h - found")
+    endif()
+  else()
+    if(NOT GTS_FIND_QUIETLY)
+      message(STATUS "Looking for gts.h gtsconfig.h - not found")
+    endif()
+
+    set(GTS_FOUND false)
+  endif()
+  mark_as_advanced(GTS_INCLUDE_DIRS)
+
+  # 2. look for GTS libraries
+  find_library(GTS_LIBRARIES gts)
+  mark_as_advanced(GTS_LIBRARIES)
+
+  if (GTS_LIBRARIES)
+    if(NOT GTS_FIND_QUIETLY)
+      message(STATUS "Looking for gts library - found")
+    endif()
+  else()
+    if(NOT GTS_FIND_QUIETLY)
+      message(STATUS "Looking for gts library - not found")
+    endif()
+
+    set (GTS_FOUND false)
+  endif()
+
+  # 2.1 Need glib library
+  find_library(GLIB_LIBRARY glib-2.0)
+  list(APPEND GTS_LIBRARIES "${GLIB_LIBRARY}")
+
+  if (GTS_FOUND)
+    include(IgnPkgConfig)
+    ign_pkg_check_modules(GTS "gts")
+    include(IgnImportTarget)
+    ign_import_target(GTS)
+  endif()
+endif()
