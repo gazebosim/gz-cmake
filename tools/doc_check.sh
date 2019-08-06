5f6f1ec7ca4393ffa4a291bd09907a173c3e1499
Nate Koenig <natekoenig@gmail.com>
2019-08-06 09:34:37 -0700

    Fix link

--- tools/doc_check.sh
+++ tools/doc_check.sh
@@ -4,7 +4,7 @@
 # that there are no doxygen warnings. For example, if you're using bitbucket
 # pipelines then you can add the following line to your bitbucket-pipelines.yml
 # file:
-#   - bash <(curl -s https://bitbucket.org/ignitionrobotics/ign-cmake/raw/3b2778025650d050d0a85f5170a0aa6b35b68bc7/tools/doc_check.sh)
+#   - bash <(curl -s https://bitbucket.org/ignitionrobotics/ign-cmake/raw/02de4e1a3690e0ea37b190f9c35ecf25e6b0e251/tools/doc_check.sh)
 if [ -s ignition-doxygen.warn ]; then
   exit 0
 else
