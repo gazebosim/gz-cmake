d98f8c35ee562121c03956d54ae0ce8c97645b61
Nate Koenig <natekoenig@gmail.com>
2019-08-06 09:33:47 -0700

    backward

--- tools/doc_check.sh
+++ tools/doc_check.sh
@@ -6,7 +6,7 @@
 # file:
 #   - bash <(curl -s https://bitbucket.org/ignitionrobotics/ign-cmake/raw/3b2778025650d050d0a85f5170a0aa6b35b68bc7/tools/doc_check.sh)
 if [ -s ignition-doxygen.warn ]; then
-  exit 1
-else
   exit 0
+else
+  exit 1
 fi
