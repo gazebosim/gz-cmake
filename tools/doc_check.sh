a2e6fdc2b9f0c7842f99eca58c002a66cc58ab95
Nate Koenig <natekoenig@gmail.com>
2019-08-06 09:08:21 -0700

    exit

--- tools/doc_check.sh
+++ tools/doc_check.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+# This file can be downloaded (such as through curl) and used in CI to check
+# that there are no doxygen warnings. For example, if you're using bitbucket
+# pipelines then you can add the following line to your bitbucket-pipelines.yml
+# file:
+#   - bash <(curl -s https://bitbucket.org/ignitionrobotics/ign-cmake/src/1a4d101cf4c1d3b8232d3da115995c99ea532ae7/tools/doc_check.sh)
 if [ -s ignition-doxygen.warn ]; then
-  return 1
+  exit 1
 else
-  return 0
+  exit 0
 fi
