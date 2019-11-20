287efcb5edbf5b9b4d51277c5fbe3df633a26aef
Jose Luis Rivero <jrivero@osrfoundation.org>
2019-11-20 19:24:03 +0100

    Reduce name for core_no_deps

--- examples/test_core_child_requires_core_no_deps.bash
+++ examples/test_core_child_requires_core_no_deps.bash
@@ -1,13 +1,12 @@
 #!/bin/bash
 if pkg-config ignition-core_child --print-requires \
-  | grep ignition-core_no_deps
+  | grep ignition-core_nodep
 then
-  echo Successfully detected ignition-core_no_deps requirement
-  cp core_child_requires_core_no_deps_pass.xml ../test_results/core_child_requires_core_no_deps.xml
+  echo Successfully detected ignition-core_nodep requirement
+  cp core_child_requires_core_nodep_pass.xml ../test_results/core_child_requires_core_nodep.xml
   exit 0
 else
-  echo Could not detect ignition-core_no_deps requirement
-  cp core_child_requires_core_no_deps_fail.xml ../test_results/core_child_requires_core_no_deps.xml
+  echo Could not detect ignition-core_nodep requirement
+  cp core_child_requires_core_nodep_fail.xml ../test_results/core_child_requires_core_nodep.xml
   exit 1
 fi
-
