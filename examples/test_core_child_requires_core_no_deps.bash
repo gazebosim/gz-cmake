fe27cfd8912ec8b6ad60747c1ee7d28feb145d3a
Steve Peters <scpeters@openrobotics.org>
2020-04-07 22:44:23 -0700

    add test illustrating bug that private dependencies are not specified as Requires.private in pkg-config files

--- examples/test_core_child_requires_core_no_deps.bash
+++ examples/test_core_child_requires_core_no_deps.bash
@@ -1,12 +1,36 @@
 #!/bin/bash
-if pkg-config ignition-core_child --print-requires \
+TEST_STATUS=0
+
+if ! pkg-config ignition-core_child --print-requires \
   | grep ignition-core_no_deps
 then
-  echo Successfully detected ignition-core_nodep requirement
+  TEST_STATUS=1
+fi
+
+if pkg-config ignition-core_child --print-requires-private \
+  | grep ignition-core_no_deps
+then
+  TEST_STATUS=1
+fi
+
+if ! pkg-config ignition-core_child_private --print-requires-private \
+  | grep ignition-core_no_deps
+then
+  TEST_STATUS=1
+fi
+
+if pkg-config ignition-core_child_private --print-requires \
+  | grep ignition-core_no_deps
+then
+  TEST_STATUS=1
+fi
+
+if [[ $TEST_STATUS ]]
+  echo Successfully detected ignition-core_nodep requirements
   cp core_child_requires_core_nodep_pass.xml ../test_results/core_child_requires_core_nodep.xml
   exit 0
 else
-  echo Could not detect ignition-core_nodep requirement
+  echo Could not detect ignition-core_nodep requirements
   cp core_child_requires_core_nodep_fail.xml ../test_results/core_child_requires_core_nodep.xml
   exit 1
 fi
