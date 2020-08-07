7419c27963c173de92e7487a4fc95d35cf253251
Jose Luis Rivero <jrivero@osrfoundation.org>
2020-08-07 18:19:40 +0200

    Add ExtraTestMacros feature (#102)
    The new file provides macros that help with filtering googletest on some of the platforms.
    
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>
    Signed-off-by: Louise Poubel <louise@openrobotics.org>

---
+++ include/ignition/utilities/ExtraTestMacros.hh
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2020 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef IGNITION_UTILITIES_EXTRATESTMACROS_HH
+#define IGNITION_UTILITIES_EXTRATESTMACROS_HH
+
+#include <ignition/utilities/detail/ExtraTestMacros.hh>
+
+/// \brief Restrict the execution of the test for the Windows platform.
+/// The test will be compiled on Windows too but will never be run as
+/// part of the test suite. The macro uses the Disabled_ prefix provided
+/// by googletest. See
+/// https://chromium.googlesource.com/external/github.com/google/googletest/+/HEAD/googletest/docs/advanced.md
+#define IGN_UTILS_TEST_DISABLED_ON_WIN32(TestName) \
+  DETAIL_IGN_UTILS_TEST_DISABLED_ON_WIN32(TestName)
+
+/// \brief Restrict the execution of the test for the Mac platform.
+/// The test will be compiled on Windows too but will never be run as
+/// part of the test suite. The macro uses the Disabled_ prefix provided
+/// by googletest. See
+/// https://chromium.googlesource.com/external/github.com/google/googletest/+/HEAD/googletest/docs/advanced.md
+#define IGN_UTILS_TEST_DISABLED_ON_MAC(TestName) \
+  DETAIL_IGN_UTILS_TEST_DISABLED_ON_MAC(TestName)
+
+/// \brief Restrict the execution of the test to just the Linux platform
+/// Other platforms will get the test compiled but it won't be run
+/// as part of the test suite execution.
+/// The macro uses the Disabled_ prefix provided by googletest. See
+/// https://chromium.googlesource.com/external/github.com/google/googletest/+/HEAD/googletest/docs/advanced.md
+#define IGN_UTILS_TEST_ENABLED_ONLY_ON_LINUX(TestName) \
+  DETAIL_IGN_UTILS_TEST_ENABLED_ONLY_ON_LINUX(TestName)
+
+#endif
---
+++ include/ignition/utilities/detail/ExtraTestMacros.hh
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2020 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef IGNITION_UTILITIES_DETAIL_EXTRATESTMACROS_HH
+#define IGNITION_UTILITIES_DETAIL_EXTRATESTMACROS_HH
+
+#include <ignition/utilities/SuppressWarning.hh>
+
+#define DETAIL_IGN_UTILS_ADD_DISABLED_PREFIX(x) DISABLED_##x
+
+#if defined _WIN32
+
+  #define DETAIL_IGN_UTILS_TEST_DISABLED_ON_WIN32(TestName) \
+      DETAIL_IGN_UTILS_ADD_DISABLED_PREFIX(TestName)
+
+#else
+
+  #define DETAIL_IGN_UTILS_TEST_DISABLED_ON_WIN32(TestName) \
+      TestName
+
+#endif
+
+#if defined __APPLE__
+
+  #define DETAIL_IGN_UTILS_TEST_DISABLED_ON_MAC(TestName) \
+      DETAIL_IGN_UTILS_ADD_DISABLED_PREFIX(TestName)
+
+#else
+
+  #define DETAIL_IGN_UTILS_TEST_DISABLED_ON_MAC(TestName) \
+      TestName
+
+#endif
+
+#if defined __linux__
+
+  #define DETAIL_IGN_UTILS_TEST_ENABLED_ONLY_ON_LINUX(TestName) \
+      TestName
+
+#else
+
+  #define DETAIL_IGN_UTILS_TEST_ENABLED_ONLY_ON_LINUX(TestName) \
+      DETAIL_IGN_UTILS_ADD_DISABLED_PREFIX(TestName)
+
+#endif
+
+
+#endif
