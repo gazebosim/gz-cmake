d1842c6bfbee8363114e6e7ae28a25fe92f24eeb
Luca Della Vedova <luca@openrobotics.org>
2022-07-29 12:09:18 +0200

    Cleanup
    Signed-off-by: Luca Della Vedova <luca@openrobotics.org>

--- cmake/FindGzAssimp.cmake
+++ cmake/FindGzAssimp.cmake
@@ -15,41 +15,10 @@
 #
 ########################################
 # Find Assimp
+# Provides a GzAssimp alias to avoid conflicts with other packages
+# that might provide a FindAssimp function, such as dartsim
 
-include(GzPkgConfig)
-
-message("Looking for assimp")
-
-#if(GzAssimp_FIND_VERSION)
-#  gz_pkg_check_modules_quiet(GzAssimp "assimp >= ${GzAssimp_FIND_VERSION}")
-#else()
-#  gz_pkg_check_modules_quiet(GzAssimp "assimp")
-#endif()
-
-#if(NOT GzAssimp_FOUND)
-if(WIN32)
-#  message("Doing manual search")
-#  include(GzManualSearch)
-#  gz_manual_search(GzAssimp
-#                   HEADER_NAMES "assimp/scene.h"
-#                   LIBRARY_NAMES "assimp"
-#		   TARGET_NAME "GzAssimp::GzAssimp")
-#  find_library(TMP_ASSIMP_LIB "assimp-vc-141-mt")
-#  message("TMP ASSIMP LIB IS ${TMP_ASSIMP_LIB}")
-#
-#   gz_pkg_config_entry(GzAssimp "assimp")
-endif()
 find_package(assimp CONFIG QUIET)
-
-message("GzAssimp found ${GzAssimp_FOUND}")
-message("GzAssimp libs ${GzAssimp_LIBRARIES}")
-message("GzAssimp include dirs ${GzAssimp_INCLUDE_DIRS}")
-message("GzAssimp version ${GzAssimp_VERSION}")
-
-message("Assimp found ${assimp_FOUND}")
-message("Assimp libs ${ASSIMP_LIBRARIES}")
-message("Assimp include dirs ${ASSIMP_INCLUDE_DIRS}")
-message("Assimp version ${assimp_VERSION}")
 
 set(GzAssimp_FOUND ${assimp_FOUND})
 set(GzAssimp_LIBRARIES ${ASSIMP_LIBRARIES})
@@ -57,9 +26,8 @@ set(GzAssimp_INCLUDE_DIRS ${ASSIMP_INCLUDE_DIRS})
 set(GzAssimp_INCLUDE_DIRS ${ASSIMP_INCLUDE_DIRS})
 set(GzAssimp_VERSION ${assimp_VERSION})
 
+include(GzPkgConfig)
 gz_pkg_config_entry(GzAssimp "assimp")
-#mark_as_advanced(GzAssimp_LIBRARY_assimp)
-#set(GzAssimp_LIBRARY_assimp ${GzAssimp_LIBRARIES})
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
