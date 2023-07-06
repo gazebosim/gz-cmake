6dfa4be7c6eaa13ed9024e04d977f0a2097e67d9
Steve Peters <scpeters@openrobotics.org>
2023-07-06 09:59:23 -0700

    Compute relative path for cmake extras (#362)
    Define a variable in gz_configure_project with the
    relative path from the extras install folder to the
    install prefix. This is needed by downstream packages.
    Also handle cmake extras after _gz_setup_packages()
    so that the GNUInstallDirs variables are available.
    
    Signed-off-by: Steve Peters <scpeters@openrobotics.org>

--- cmake/GzConfigureProject.cmake
+++ cmake/GzConfigureProject.cmake
@@ -153,6 +153,24 @@ macro(gz_configure_project)
     list(APPEND extras ${gz_configure_project_CONFIG_EXTRAS})
   endif()
 
+  #============================================================================
+  # Identify the operating system
+  gz_check_os()
+
+  #============================================================================
+  # Create package information
+  _gz_setup_packages()
+
+  #============================================================================
+  # Configure and install cmake extras files
+  # Do this after _gz_setup_packages() to ensure GNUInstallDirs has been called
+  set(PROJECT_CMAKE_EXTRAS_INSTALL_DIR ${CMAKE_INSTALL_FULL_LIBDIR}/cmake/${PROJECT_NAME})
+  file(RELATIVE_PATH
+    PROJECT_CMAKE_EXTRAS_PATH_TO_PREFIX
+    "${PROJECT_CMAKE_EXTRAS_INSTALL_DIR}"
+    "${CMAKE_INSTALL_PREFIX}"
+  )
+
   foreach(extra ${extras})
     _gz_assert_file_exists("${extra}"
       "gz_configure_project() called with extra file '${extra}' which does not exist")
@@ -180,7 +198,7 @@ macro(gz_configure_project)
     if(is_cmake)
       install(FILES
         ${extra}
-        DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME}/
+        DESTINATION ${PROJECT_CMAKE_EXTRAS_INSTALL_DIR}
       )
       get_filename_component(extra_filename "${extra}" NAME)
       list(APPEND PACKAGE_CONFIG_EXTRA_FILES "${extra_filename}")
@@ -189,14 +207,6 @@ macro(gz_configure_project)
         "does neither end with '.cmake' nor with '.cmake.in'.")
     endif()
   endforeach()
-
-  #============================================================================
-  # Identify the operating system
-  gz_check_os()
-
-  #============================================================================
-  # Create package information
-  _gz_setup_packages()
 
   #============================================================================
   # Initialize build errors/warnings
