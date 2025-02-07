bbaba83ba11d7c8b6924c1f2e089abe00e1f343d
Steve Peters <scpeters@openrobotics.org>
2025-02-07 15:12:13 -0800

    Normalize header install path (#467)
    The "meta" header containing a list of all includes in
    the package is currently installed one folder above the
    other includes. This cleans up the definition of that
    install path by normalizing it to fix a cmake warning
    and also renaming some variables for clarity.
    
    Signed-off-by: Steve Peters <scpeters@openrobotics.org>
    (cherry picked from commit 5b556fb2a915272a1b888d7bc762794f1112c59b)

--- cmake/GzInstallAllHeaders.cmake
+++ cmake/GzInstallAllHeaders.cmake
@@ -185,31 +185,36 @@ function(gz_install_all_headers)
 
     set(component_name ${gz_install_all_headers_COMPONENT})
 
-    # Define the install directory for the component meta header
-    set(meta_header_install_dir ${GZ_INCLUDE_INSTALL_DIR_FULL}/${PROJECT_INCLUDE_DIR}/${component_name})
+    # Define the install directory for the "config" header
+    # The "meta" header will be installed one folder above this
+    set(config_header_install_dir ${GZ_INCLUDE_INSTALL_DIR_FULL}/${PROJECT_INCLUDE_DIR}/${component_name})
 
-    # Define the input/output of the configuration for the component "master" header
-    set(master_header_in ${GZ_CMAKE_DIR}/gz_auto_headers.hh.in)
-    set(master_header_out ${CMAKE_CURRENT_BINARY_DIR}/${component_name}.hh)
+    # Define the input/output of the configuration for the component "meta" header
+    set(meta_header_in ${GZ_CMAKE_DIR}/gz_auto_headers.hh.in)
+    set(meta_header_out ${CMAKE_CURRENT_BINARY_DIR}/${component_name}.hh)
 
   else()
 
-    # Define the install directory for the core master meta header
-    set(meta_header_install_dir ${GZ_INCLUDE_INSTALL_DIR_FULL}/${PROJECT_INCLUDE_DIR})
+    # Define the install directory for the "config" header
+    # The core "meta" header will be installed one folder above this
+    set(config_header_install_dir ${GZ_INCLUDE_INSTALL_DIR_FULL}/${PROJECT_INCLUDE_DIR})
 
-    # Define the input/output of the configuration for the core "master" header
-    set(master_header_in ${GZ_CMAKE_DIR}/gz_auto_headers.hh.in)
-    set(master_header_out ${CMAKE_CURRENT_BINARY_DIR}/../${GZ_DESIGNATION}.hh)
+    # Define the input/output of the configuration for the core "meta" header
+    set(meta_header_in ${GZ_CMAKE_DIR}/gz_auto_headers.hh.in)
+    set(meta_header_out ${CMAKE_CURRENT_BINARY_DIR}/../${GZ_DESIGNATION}.hh)
 
   endif()
 
-  # Generate the "master" header that includes all of the headers
-  configure_file(${master_header_in} ${master_header_out})
+  # Generate the install directory for the "meta" header one folder above the "config" header
+  cmake_path(SET meta_header_install_dir NORMALIZE ${config_header_install_dir}/..)
 
-  # Install the "master" header
+  # Generate the "meta" header that includes all of the headers
+  configure_file(${meta_header_in} ${meta_header_out})
+
+  # Install the "meta" header
   install(
-    FILES ${master_header_out}
-    DESTINATION ${meta_header_install_dir}/..
+    FILES ${meta_header_out}
+    DESTINATION ${meta_header_install_dir}
     COMPONENT headers)
 
   # Define the input/output of the configuration for the "config" header
@@ -235,7 +240,7 @@ function(gz_install_all_headers)
     # Install the "config" header
     install(
       FILES ${config_header_out}
-      DESTINATION ${meta_header_install_dir}
+      DESTINATION ${config_header_install_dir}
       COMPONENT headers)
 
   endif()
