a3d8291d58f58fee04c81fc4da769fb98059aabb
Jose Luis Rivero <jrivero@honurobotics.com>
2025-02-17 18:13:29 +0100

    Port test to gz
    Signed-off-by: Jose Luis Rivero <jrivero@honurobotics.com>

--- examples/test_c_child_requires_c_no_deps.bash
+++ examples/test_c_child_requires_c_no_deps.bash
@@ -2,40 +2,40 @@
 TEST_STATUS=0
 
 echo
-echo Expect ignition-c_child to require ignition-c_no_deps
-pkg-config ignition-c_child --print-requires
-if ! pkg-config ignition-c_child --print-requires \
-  | grep ignition-c_no_deps
+echo Expect gz-c_child to require gz-c_no_deps
+pkg-config gz-c_child --print-requires
+if ! pkg-config gz-c_child --print-requires \
+  | grep gz-c_no_deps
 then
   echo oops
   TEST_STATUS=1
 fi
 
 echo
-echo Expect ignition-c_child to not privately require ignition-c_no_deps
-pkg-config ignition-c_child --print-requires-private
-if pkg-config ignition-c_child --print-requires-private \
-  | grep ignition-c_no_deps
+echo Expect gz-c_child to not privately require gz-c_no_deps
+pkg-config gz-c_child --print-requires-private
+if pkg-config gz-c_child --print-requires-private \
+  | grep gz-c_no_deps
 then
   echo oops
   TEST_STATUS=1
 fi
 
 echo
-echo Expect ignition-c_child_private to privately require ignition-c_no_deps
-pkg-config ignition-c_child_private --print-requires-private
-if ! pkg-config ignition-c_child_private --print-requires-private \
-  | grep ignition-c_no_deps
+echo Expect gz-c_child_private to privately require gz-c_no_deps
+pkg-config gz-c_child_private --print-requires-private
+if ! pkg-config gz-c_child_private --print-requires-private \
+  | grep gz-c_no_deps
 then
   echo oops
   TEST_STATUS=1
 fi
 
 echo
-echo Expect ignition-c_child_private to not require ignition-c_no_deps
-pkg-config ignition-c_child_private --print-requires
-if pkg-config ignition-c_child_private --print-requires \
-  | grep ignition-c_no_deps
+echo Expect gz-c_child_private to not require gz-c_no_deps
+pkg-config gz-c_child_private --print-requires
+if pkg-config gz-c_child_private --print-requires \
+  | grep gz-c_no_deps
 then
   echo oops
   TEST_STATUS=1
@@ -44,11 +44,11 @@ fi
 echo
 if [[ $TEST_STATUS -eq 0 ]]
 then
-  echo Successfully detected ignition-c_nodep requirements
+  echo Successfully detected gz-c_nodep requirements
   cp c_child_requires_c_nodep_pass.xml ../test_results/c_child_requires_c_nodep.xml
   exit 0
 else
-  echo Could not detect all ignition-c_nodep requirements correctly
+  echo Could not detect all gz-c_nodep requirements correctly
   cp c_child_requires_c_nodep_fail.xml ../test_results/c_child_requires_c_nodep.xml
   exit 1
 fi
